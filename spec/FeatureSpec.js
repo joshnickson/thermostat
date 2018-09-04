04894fa4fcdcdfd93ab97bb89adb6ef4bde65546
josh nickson <jnickson1506@gmail.com>
2018-09-04 19:48:52 +0100

    add up function to raise temp

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -9,5 +9,10 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(20)
   });
 
+  it("should increase the temperature with an up function", function() {
+    thermostat.up(10)
+    expect(thermostat.temperature).toEqual(30)
+  });
+
 });
 
--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,8 +1,16 @@
 describe("Thermostat", function() {
   var thermostat;
 
-  beforeEach(function() {
-    thermostat = new Thermostat();
+  beforeEach(function(){
+    thermostat = new Thermostat;
   });
+
+  describe("Up", function() {
+    it("should increase temperature", function() {
+      thermostat.up(10)
+      expect(thermostat.temperature).toEqual(30)
+    });
+  });
+
 });
 
