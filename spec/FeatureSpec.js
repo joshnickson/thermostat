1797266686f0caaceba765f347ff2e22689c9927
josh nickson <jnickson1506@gmail.com>
2018-09-04 20:01:34 +0100

    set min temperature for thermostat

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -6,17 +6,22 @@ describe("Thermostat", function() {
   });
 
   it("should have an initial start temperature of 20", function() {
-    expect(thermostat.temperature).toEqual(20)
+    expect(thermostat.temperature).toEqual(20);
   });
 
   it("should increase the temperature with an up function", function() {
-    thermostat.up(10)
-    expect(thermostat.temperature).toEqual(30)
+    thermostat.up(10);
+    expect(thermostat.temperature).toEqual(30);
   });
 
   it("should decrease the temperature with an down function", function() {
-    thermostat.down(10)
-    expect(thermostat.temperature).toEqual(10)
+    thermostat.down(10);
+    expect(thermostat.temperature).toEqual(10);
+  });
+
+  it("has a minumum temp of 10 degrees", function() {
+    thermostat.down(20);
+    expect(thermostat.temperature).toEqual(10);
   });
 
 });
