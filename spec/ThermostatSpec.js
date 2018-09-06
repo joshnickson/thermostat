f6cf6c251da86446bd51010b4d21b7449f6b0a84
josh nickson <jnickson1506@gmail.com>
2018-09-06 09:28:28 +0100

    move tests to ThermostatSpec.js

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe("Thermostat", function() {
   var thermostat;
 
@@ -5,18 +7,48 @@ describe("Thermostat", function() {
     thermostat = new Thermostat;
   });
 
-  describe("Up", function() {
-    it("should increase temperature", function() {
-      thermostat.up(2)
-      expect(thermostat.temperature).toEqual(22)
-    });
+  it("should have an initial start temperature of 20", function() {
+    expect(thermostat.temperature).toEqual(20);
   });
 
-  describe("Down", function() {
-    it("should increase temperature", function() {
-      thermostat.down(10)
-      expect(thermostat.temperature).toEqual(10)
-    });
+  it("should increase the temperature with an up function", function() {
+    thermostat.up(2);
+    expect(thermostat.temperature).toEqual(22);
   });
 
+  it("should decrease the temperature with an down function", function() {
+    thermostat.down(10);
+    expect(thermostat.temperature).toEqual(10);
+  });
+
+  it("has a minumum temp of 10 degrees", function() {
+    thermostat.down(20);
+    expect(thermostat.temperature).toEqual(10);
+  });
+
+  it("has max temp 25 when power saving mode enabled", function() {
+    thermostat.up(20);
+    expect(thermostat.temperature).toEqual(25);
+  });
+
+  it("has max temp 32 when power saving mode disabled", function() {
+    thermostat.powerSave = 'off';
+    thermostat.up(20);
+    expect(thermostat.temperature).toEqual(32);
+  });
+
+  it("can reset the temperature to 20", function() {
+    thermostat.up(5)
+    thermostat.resetTemp();
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it("can display information on energy usage", function() {
+    expect(thermostat.energyUsage()).toEqual('medium-usage');
+    thermostat.temperature = 25
+    expect(thermostat.energyUsage()).toEqual('high-usage');
+    thermostat.temperature = 15
+    expect(thermostat.energyUsage()).toEqual('low-usage');
+  });
 });
+
